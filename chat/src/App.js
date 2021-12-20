1a1c2caff9dd6a1001c88354da84f7b50eb0fea2
Bharat Goyal <bgoyal1471@gmail.com>
2021-12-20 19:08:28 +0530

    feat: Added Icons

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     // BEM naming convention
     <div className="App">
-     <h1>Hello BharatDev, Let's build real time chat application </h1>
+      <h1>Hello BharatDev, Let's build real time chat application </h1>
 
-     {/* Header */}
-     {/* Sidebar */}
-     {/* React-Router --> Chat screen */}
+      {/* Header */}
+
+      <Header />
+      {/* Sidebar */}
+      {/* React-Router --> Chat screen */}
     </div>
   );
 }
--- chat/src/Header.js
+++ chat/src/Header.js
@@ -1,26 +1,34 @@
 import React from "react";
 import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@mui/icons-material/AccessAlarm";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import HelpOutlineSharpIcon from "@mui/icons-material/HelpOutlineSharp";
 
 function Header() {
-  return <div className="header">
-  <div className="header_left"></div>
-  
-  {/* Avatar for logged in user */}
-  {/* Time icon */}
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* Avatar for logged in user */}
+        <Avatar className="header__avatar" alt="Bharat Goyal" src="" />
 
-  <div className="header_search">
+        <AccessTimeIcon />
+
+        {/* Time icon */}
+      </div>
+
+      <div className="header__search">
         {/* Search Icon */}
+        <SearchSharpIcon />
         {/*Input */}
-       
-  </div>
+        <input placeholder="Search mechbg" />
+      </div>
 
-  <div className="header_left">
-        {/* help icon */}
-  </div>
-  
-  
-  </div>;
-  
+      <div className="header__right">
+        <HelpOutlineSharpIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
